6ecb1e487b9108570753eee7ac7b86cb7a0eebe1
MwitaBrian <brianmwita13@gmail.com>
2023-03-03 05:30:59 +0300

    Completed

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,21 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def character_list
+    self.characters.map(&:name)
+  end
+
+  def show_list
+    self.shows.map(&:name)
+  end
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    self.characters.map do |character|
+      "#{character.name} - #{character.show.name}"
+    end
+  end
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,16 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :actor
+  belongs_to :show
+ 
+  def actor_object
+    self.actor
+  end
+
+  def show_object
+    self.show
+  end
+
+  def say_that_thing_you_say
+    "#{name} always says: #{catchphrase}"
+  end
 end
